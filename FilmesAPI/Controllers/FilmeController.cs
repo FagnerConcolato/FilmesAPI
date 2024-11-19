dc599a8fd7ef7b4d14422c4d62093f111112bbaf
FagnerConcolato <fca.fisica@gmail.com>
2024-11-19 10:54:34 -0400

    Criando a estrutura do projeto, Controllers e Models

---
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -0,0 +1,20 @@
+﻿using FilmesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmesAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FilmeController : ControllerBase
+
+    {
+        private static List<Filme> Filmes = new List<Filme>();
+        [HttpPost]
+        public void AdicionalFilme([FromBody]Filme filme)
+        {
+            Filmes.Add(filme);
+            Console.WriteLine(filme.Titulo);
+            Console.WriteLine(filme.Duracao);
+        }
+    }
+}
